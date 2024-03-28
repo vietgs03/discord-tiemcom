60261cd9e109b7f7978414f9980f0fb7c7e28dce
Hoàng Xuân Việt <129947921+vietgs03@users.noreply.github.com>
2024-03-28 14:54:09 +0700

    commit

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,8 +8,9 @@ const MainLayout =async ({
     return ( 
         <div className="h-full">
             <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+                {/* @ts-expect-error Server Component */}
                 <NavigationSidebar
-                
+ 
                 />
             </div>
             <main className="md:pl-[72px] h-full"> 
